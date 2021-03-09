6169a3c5dcb985e8e98f8951d7ea4b10549fb94d
Thanikul Sriuthis <moremeng@dv4.biz>
2021-03-09 14:24:26 +0700

    fixed: deprecated

--- _scripts/script.js
+++ _scripts/script.js
@@ -1,24 +1,34 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('./models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('./models')
 ]).then(startVideo)
 
 function startVideo() {
-  navigator.getUserMedia(
-    { video: {} },
-    stream => video.srcObject = stream,
-    err => console.error(err)
-  )
+  // navigator.getUserMedia({
+  //     video: {}
+  //   },
+  //   stream => video.srcObject = stream,
+  //   err => console.error(err)
+  // )
+
+  navigator.mediaDevices.getUserMedia({
+    video: {}
+  }).then((stream) => {
+    video.srcObject = stream;
+  }, (err) => console.error(err));
 }
 
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
+  const displaySize = {
+    width: video.width,
+    height: video.height
+  }
   faceapi.matchDimensions(canvas, displaySize)
   setInterval(async () => {
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
